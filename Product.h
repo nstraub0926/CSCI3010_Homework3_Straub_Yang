825236f103aa9d1eca0348c90ae8de6d5492d982
Peter Yang <yuya0723@colorado.edu>
2022-03-11 18:31:13 -0700

    Complete methods in ProductFactory

--- Product.h
+++ Product.h
@@ -42,9 +42,8 @@ class Television : public Electronics {
 
 class Phone : public Electronics {
  public:
-   Phone(std::string name, double basePrice, std::string quality, std::string seller) : Electronics(name, basePrice, quality, seller){};
+  Phone(std::string name, double basePrice, std::string quality, std::string seller) : Electronics(name, basePrice, quality, seller){};
 };
-
 
 class Computers : public Product {
  public:
@@ -66,7 +65,6 @@ class Keyboard : public Computers {
   Keyboard(std::string name, double basePrice, std::string quality, std::string seller) : Computers(name, basePrice, quality, seller){};
 };
 
-
 class Sports : public Product {
  public:
   Sports(std::string name, double basePrice, std::string quality, std::string seller) : Product(name, basePrice, quality, seller){};
@@ -87,7 +85,6 @@ class Baseball : public Sports {
   Baseball(std::string name, double basePrice, std::string quality, std::string seller) : Sports(name, basePrice, quality, seller){};
 };
 
-
 class Clothing : public Product {
  public:
   Clothing(std::string name, double basePrice, std::string quality, std::string seller) : Product(name, basePrice, quality, seller){};
@@ -100,14 +97,13 @@ class MenClothing : public Clothing {
 
 class WomenClothing : public Clothing {
  public:
-  MenClothing(std::string name, double basePrice, std::string quality, std::string seller) : Clothing(name, basePrice, quality, seller){};
+  WomenClothing(std::string name, double basePrice, std::string quality, std::string seller) : Clothing(name, basePrice, quality, seller){};
 };
 
 class Necklaces : public Clothing {
  public:
-  MenClothing(std::string name, double basePrice, std::string quality, std::string seller) : Clothing(name, basePrice, quality, seller){};
+  Necklaces(std::string name, double basePrice, std::string quality, std::string seller) : Clothing(name, basePrice, quality, seller){};
 };
-
 
 class Books : public Product {
  public:
@@ -121,30 +117,29 @@ class Textbooks : public Books {
 
 class Novels : public Books {
  public:
-  Textbooks(std::string name, double basePrice, std::string quality, std::string seller) : Books(name, basePrice, quality, seller){};
+  Novels(std::string name, double basePrice, std::string quality, std::string seller) : Books(name, basePrice, quality, seller){};
 };
 
 class Cookbooks : public Books {
  public:
-  Textbooks(std::string name, double basePrice, std::string quality, std::string seller) : Books(name, basePrice, quality, seller){};
+  Cookbooks(std::string name, double basePrice, std::string quality, std::string seller) : Books(name, basePrice, quality, seller){};
 };
-
 
 class ProductFactory {
  public:
   static Camera* AddCamera(std::string name, double basePrice, std::string quality, std::string seller) { return new Camera(name, basePrice, quality, seller); };
-  static Television* AddTelevision();
-  static Phone* AddPhone();
-  static Monitors* AddMonitors();
-  static Mouse* AddMouse();
-  static Keyboard* AddKeyboard();
-  static Basketball* AddBasketball();
-  static Football* AddFootball();
-  static Baseball* AddBaseball();
-  static MenClothing* AddMenClothing();
-  static WomenClothing* AddWomenClothing();
-  static Necklace* AddNecklace();
-  static Textbooks* AddTextbooks();
-  static Novels* AddNovels();
-  static Cookbooks* AddCookbooks();
+  static Television* AddTelevision(std::string name, double basePrice, std::string quality, std::string seller) { return new Television(name, basePrice, quality, seller); };
+  static Phone* AddPhone(std::string name, double basePrice, std::string quality, std::string seller) { return new Phone(name, basePrice, quality, seller); };
+  static Monitors* AddMonitors(std::string name, double basePrice, std::string quality, std::string seller) { return new Monitors(name, basePrice, quality, seller); };
+  static Mouse* AddMouse(std::string name, double basePrice, std::string quality, std::string seller) { return new Mouse(name, basePrice, quality, seller); };
+  static Keyboard* AddKeyboard(std::string name, double basePrice, std::string quality, std::string seller) { return new Keyboard(name, basePrice, quality, seller); };
+  static Basketball* AddBasketball(std::string name, double basePrice, std::string quality, std::string seller) { return new Basketball(name, basePrice, quality, seller); };
+  static Football* AddFootball(std::string name, double basePrice, std::string quality, std::string seller) { return new Football(name, basePrice, quality, seller); };
+  static Baseball* AddBaseball(std::string name, double basePrice, std::string quality, std::string seller) { return new Baseball(name, basePrice, quality, seller); };
+  static MenClothing* AddMenClothing(std::string name, double basePrice, std::string quality, std::string seller) { return new MenClothing(name, basePrice, quality, seller); };
+  static WomenClothing* AddWomenClothing(std::string name, double basePrice, std::string quality, std::string seller) { return new WomenClothing(name, basePrice, quality, seller); };
+  static Necklaces* AddNecklaces(std::string name, double basePrice, std::string quality, std::string seller) { return new Necklaces(name, basePrice, quality, seller); };
+  static Textbooks* AddTextbooks(std::string name, double basePrice, std::string quality, std::string seller) { return new Textbooks(name, basePrice, quality, seller); };
+  static Novels* AddNovels(std::string name, double basePrice, std::string quality, std::string seller) { return new Novels(name, basePrice, quality, seller); };
+  static Cookbooks* AddCookbooks(std::string name, double basePrice, std::string quality, std::string seller) { return new Cookbooks(name, basePrice, quality, seller); };
 };
